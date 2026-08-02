b6e0150e0becd816bac7e44e41122fa7e32e8ccf
atikurzamanpallob <atikurzamanpallob@users.noreply.github.com>
2026-08-02 21:03:15 +0000

    deploy: atikurzamanpallob/myporftolio@daba398316faa29f9b07e71782f474e47e275edf

--- README.md
+++
@@ -1,7 +0,0 @@
-#  Portfolio Web app
-
-#### Live Link
-[https://atikurzamanpallob.github.io](https://atikurzamanpallob.github.io)
-
-![description](https://ycxmalbladgeeamoobda.supabase.co/storage/v1/object/public/others/homepage.jpg)
-
--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1484613167" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1008755486" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
