aedb7c11957ca302f9206ba0309727bbadc05399
atikurzamanpallob <atikurzamanpallob@users.noreply.github.com>
2026-08-03 11:00:54 +0000

    deploy: atikurzamanpallob/myporftolio@83a9958627554187b5485f4b9bb18869f3ce3696

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1817919884" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4120791075" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
